ed01b6aaba71dfcead681b1d42ae4340a91c5a41
Riza Fahmi <rizafahmi@gmail.com>
2020-03-16 06:12:24 +0700

    Add auth module and nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './Navigation.css';
+
+const Navigation = ({ currentUser, handleLogout, handleLogin }) => {
+  const loggedIn = !!currentUser;
+
+  return (
+    <header className="sticky">
+      <div className="container">
+        <div className="nav-brand">
+          <a href="/">
+            <h1>
+              <i className="snes-jp-logo brand-logo" /> Quote.app
+            </h1>
+          </a>
+        </div>
+        <div className="auth">
+          {!loggedIn && (
+            <a href="/" onClick={handleLogin}>
+              Login
+            </a>
+          )}
+          {loggedIn && (
+            <a href="/" onClick={handleLogout}>
+              Logout
+            </a>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const mapState = (state) => ({
+  currentUser: selectCurrentUser(props)
+});
+const mapDispatch = {
+  handleLogin: LoginUtil.handleLogin,
+  handleLogout: LoginUtil.handleLogout
+};
+connect(mapState, mapDispatch);
+
+export default Navigation;
